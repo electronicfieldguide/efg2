f7cd9bef5e8f7d428b15798594a9915197977bb6
kasiedu <kasiedu@5c346ea1-44cb-408f-a09f-fe17c537c4b5>
2006-06-08 12:57:30 +0000

    New css files

--- lib/testImport.bat
+++ lib/testImport.bat
@@ -4,10 +4,11 @@
 set LOG4J=log4j-1.2.8.jar
 set MYSQL_DRIVER=mysqldriver.jar
 set IMPORT_JAR=rdbImport.jar
+set OSTER_JAR=ostermillerutils_1_05_00_for_java_1_4.jar
 set _CP=%CLASSPATH%
 set CP=%CLASSPATH%
 set POINT=.
-set CP=%CP%;%LOG4J%;%IMPORT_JAR%;%CD%;%POINT%;%MYSQL_DRIVER%
+set CP=%CP%;%LOG4J%;%IMPORT_JAR%;%CD%;%POINT%;%MYSQL_DRIVER%;%OSTER_JAR%
 set CLASSPATH=%CP%
 
 
