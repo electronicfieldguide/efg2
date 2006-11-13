cad702ccc67cbde9d46044fa81eeb6f672bf6b04
kasiedu <kasiedu@5c346ea1-44cb-408f-a09f-fe17c537c4b5>
2006-11-13 19:10:14 +0000

    no message

--- lib/login.bat
+++ lib/login.bat
@@ -1,7 +1,5 @@
 @echo off
-
-
-
+set XERCES_JAR=xercesImpl.jar
 set LOG4J=log4j-1.2.8.jar
 set OSCACHE=oscache-2.3.jar
 set CASTOR=castor-0.9.5.2.jar
@@ -14,14 +12,15 @@ set ROWSET=rowset.jar
 set SERVLET_JAR=servlet-api.jar
 set OSTER_JAR=ostermillerutils_1_05_00_for_java_1_4.jar
 set COMMONS=commons-logging.jar
+set SPRING_CONFIG_HOME=springConfig
 set _CP=%CLASSPATH%
 set CP=%CLASSPATH%
 set POINT=.
-set CP=%CP%;%OSCACHE%;%SERVLET_JAR%;%CASTOR%;%LOG4J%;%common_logs%;%common_codec%;%IMPORT_JAR%;%CD%;%POINT%;%MYSQL_DRIVER%;%SPRING%;%ROWSET%;%OSTER_JAR%;%COMMONS;.;properties%
+set CP=%XERCES_JAR%;%OSCACHE%;%SERVLET_JAR%;%CASTOR%;%LOG4J%;%common_logs%;%common_codec%;%IMPORT_JAR%;%CD%;%POINT%;%MYSQL_DRIVER%;%SPRING%;%ROWSET%;%OSTER_JAR%;%COMMONS%;properties;%SPRING_CONFIG_HOME%;%CP%
 set CLASSPATH=%CP%
 set cat=%CATALINA_HOME%
 
-echo %cat%
+
 
 
 java -cp ".;%CLASSPATH%" project/efg/Imports/efgImpl/LoginDialog "%cat%"
