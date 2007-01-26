4946a191eeadba543346a52f4564cea92ce1d004
kasiedu <kasiedu@5c346ea1-44cb-408f-a09f-fe17c537c4b5>
2007-01-26 00:10:14 +0000

    *** empty log message ***

--- lib/login.sh
+++ lib/login.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-#$Id$
-
-java -classpath "oscache-2.3.jar:servlet-api.jar:castor-0.9.5.2.jar:log4j-1.2.8.jar:commons-logging.jar:rdbImport.jar:mysqldriver.jar:spring.jar:rowset.jar:ostermillerutils_1_05_00_for_java_1_4.jar:commons-codec-1.3.jar:.:./properties" project/efg/Imports/efgImpl/LoginDialog "$CATALINA_HOME"
+java -classpath ".:xercesImpl.jar:xerces.jar:xalan.jar:xml-apis.jar:commons-io-1.2.jar:log4j-1.2.8.jar:oscache-2.3.jar:castor-0.9.5.2.jar:commons-logging.jar:commons-codec-1.3.jar:mysqldriver.jar:rdbImport.jar:spring.jar:rowset.jar:servlet-api.jar:ostermillerutils_1_05_00_for_java_1_4.jar:commons-logging.jar:springConfig" project/efg/Imports/efgImpl/LoginDialog "$CATALINA_HOME" 
 
