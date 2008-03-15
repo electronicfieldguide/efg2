345f3b46f55d64d07194cf744fc4990212992a4c
kasiedu <kasiedu@5c346ea1-44cb-408f-a09f-fe17c537c4b5>
2008-03-15 18:22:23 +0000

    Updating login file for linux/unix. created a new login file for bdei

---
+++ lib/client/efg2Login.sh
@@ -0,0 +1,2 @@
+/tools/jdk-1.5.0_08/usr/jdk/jdk1.5.0_08/bin/java -classpath ".:xercesImpl.jar:xerces.jar:xalan.jar:xml-apis.jar:commons-io-1.2.jar:log4j-1.2.8.jar:oscache-2.3.jar:castor-0.9.5.2.jar:commons-logging.jar:commons-codec-1.3.jar:mysqldriver.jar:efg2Client.jar:spring.jar:rowset.jar:efgTemplate.jar:efgDocument.jar:ostermillerutils_1_05_00_for_java_1_4.jar:springConfig" project/efg/client/impl/gui/LoginDialog "$CATALINA_BASE" 
+
